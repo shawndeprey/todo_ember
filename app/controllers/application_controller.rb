708d016aa4c60c39f0094615ca7b9470025528ee
Shawn Deprey <shawndeprey@gmail.com>
2014-04-21 16:54:17 -0700

    Set up base routes, styles and layouts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include ApplicationHelper
+
+  def render_not_found
+    redirect_to root_path, alert: "We couldn't find the page you were looking for. Sorry about that!"
+  end
+
+  def not_found
+    raise ActionController::RoutingError.new("We couldn't find the page you were looking for. Sorry about that!")
+  end
 end
---
+++ app/controllers/default_controller.rb
@@ -0,0 +1,5 @@
+class DefaultController < ApplicationController
+  # GET /
+  def index
+  end
+end
