434326760282d225523197f81e7cb4976596ba7a
Shawn Deprey <shawndeprey@gmail.com>
2014-04-21 17:22:01 -0700

    Added fixture data for ember app.

--- app/assets/javascripts/todo_app.js
+++ app/assets/javascripts/todo_app.js
@@ -1,1 +1,5 @@
+// Create the Ember application
 window.Todos = Ember.Application.create();
+
+// Tell the ember app where it's data will come from, in this example we use fixture data
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
