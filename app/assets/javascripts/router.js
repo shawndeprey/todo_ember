eda96a41de23596a908ea949e368332354a4c4c5
Shawn Deprey <shawndeprey@gmail.com>
2014-04-23 10:58:24 -0700

    Added completed todos route.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,6 +2,7 @@ Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function() {
     // additional child routes    
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -18,6 +19,17 @@ Todos.TodosActiveRoute = Ember.Route.extend({
       return !todo.get('isCompleted');
     });
     //return this.store.find('todo');
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
+    });
   },
   renderTemplate: function(controller) {
     this.render('todos/index', {controller: controller});
