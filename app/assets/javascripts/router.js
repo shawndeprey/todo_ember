3552c91c462e147be048174ec4eb2ffd390695ba
Shawn Deprey <shawndeprey@gmail.com>
2014-04-23 10:47:44 -0700

    Added ability to only see active todos.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,7 @@
-Todos.Router.map(function () {
-  this.resource('todos', { path: '/' }, function () {
-    // additional child routes will go here later
+Todos.Router.map(function() {
+  this.resource('todos', { path: '/' }, function() {
+    // additional child routes    
+    this.route('active');
   });
 });
 
@@ -8,5 +9,17 @@ Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
     //return this.modelFor('todos');
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+    //return this.store.find('todo');
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
